e576ba6b7b7d40fe49f6d6da3eebda5b6ca34e0a
nosianuri <nurinosia@gmail.com>
2022-11-17 01:59:38 +0600

    Added services & about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './About.css';
+import about from '../../assets/about.jfif';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div className='about'>
+            <div class="card mb-3 container" >
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img className='about-image' src={about} class="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">CAN I CLAIM COMPENSATION FOR TOXIC WATER RELATED ILLNESSES AT CAMP LEJEUNE?</h5>
+                            <p class="card-text">Water at the Marine Corps camp in Jacksonville, North Carolina, was found to have been contaminated with benzene, trichloroethylene and perchloroethylene among others between 1953 to 1987. In the time since, residents of the base have experienced severe negative side-effects, like Cancer, Renal Toxicity, Infertitlity, Parkinson’s, Myeloma and many more.</p>
+                            <p class="card-text"><small class="text-muted">The U.S. Department of Veteran Affairs (VA) tried to avoid helping US Marines and their families, but a new bipartisan bill allows our heroes to get the help they deserve. Our specialist lawyers can help you and your family get the maximum compensation you deserve.</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default About
+export default About;
--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from 'react';
+import img1 from '../../assets/img1.jpg';
+import img2 from '../../assets/img2.jpg';
+import img3 from '../../assets/img3.jpg';
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+    return (
+        <div>
+            <div>
+            <h1>3 SIMPLE STEPS TO MAKING A CAMP LEJEUNE CLAIM</h1>
+            <div className='container'>
+            <div class="row row-cols-1 row-cols-md-3 g-4 ">
+                <div class="col">
+                    <div class="card">
+                        <img src={img1} class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">ONLINE ELIGIBILITY CHECK</h5>
+                            <p class="card-text">TThe easiest way to find out and start the process is to answer a few simple questions via our online form. We can check today if you are eligble to receive a settlement figure for your suffering.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src={img2} class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">FREE EXPERT ADVICE</h5>
+                            <p class="card-text">Our attorneys offer a free initial chat to discuss your illmesses and eligibility. They can then assess how much compensation you should be owed, taking into account medical bills etc.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <img src={img3} class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <h5 class="card-title">MAXIMUM COMPENSATION</h5>
+                            <p class="card-text">We believe everyone that has been affected is due the maximum compensation for their suffering. With all the information and your authority we ensure we get you the maximum settlement you deserve.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+            </div>
+        </div>
+    )
 }
 
 export default Blog
