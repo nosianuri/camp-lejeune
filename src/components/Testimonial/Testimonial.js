c0bec47d61c87c7814beeda47315101b69eb0bf6
nosianuri <nurinosia@gmail.com>
2022-11-17 02:14:24 +0600

    added tstimonial

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './testimonial.css';
 
 const Testimonial = () => {
   return (
-    <div>Testimonial</div>
+    <div className='testimonials'>
+         <div className='container'>
+      <div className="gpt3__cta">
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+    </div>
+    <div className="gpt3__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+    </div>
+    </div>
   )
 }
 
